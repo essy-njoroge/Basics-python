79acfcdc3f475d945dfa95a714d2797156afd177
essy-njoroge <esthernnjoroge18@gmail.com>
2026-02-04 07:11:26 +0300

    initial commit

---
+++ introduction.py
@@ -0,0 +1,7 @@
+#print is a method used to display an output
+
+print( "Welcome To Python Programming")
+print( "Esther")
+print(200)
+print(40*3)
+
---
+++ variables.py
@@ -0,0 +1,22 @@
+# variable is a container used to store a value
+
+school = "eMobilis"
+firstname = "Esther"
+lastname = "Njoroge"
+num1 =45
+num2 =100
+language = "python"
+
+
+print(school)
+print(firstname)
+print(lastname)
+print(firstname,lastname)
+print(num1)
+print(num2)
+print(language)
+print(school,"is a coding school")
+print(language,"is a web programming language")
+print("The sum of",num1,"and",num2,"is",num1+num2)
+
+
