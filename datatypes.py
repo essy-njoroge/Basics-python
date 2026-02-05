fb384d40c8e942e5c6785f6bd8430925ce98b48b
essy-njoroge <esthernnjoroge18@gmail.com>
2026-02-05 08:42:26 +0300

    new commit

---
+++ datatypes.py
@@ -0,0 +1,52 @@
+age = 56  #integers
+length = 45.6 #float
+greeting = "Hello there"  #string
+hasfeathers = True  #boolean
+
+#Data structures-multiple value stored in one variable.
+
+fruits = ["banana","apple","mango"]  #list - ordered and changeable and different datatypes
+courses = ["MIT","DataScience","CyberSecurity"]   #array - similar datatypes
+cars = ("Mecedes","Ford","Nissan","G-wagon")   #taple - ordered and unchangeable
+countries = {"Kenya","Zambia","Canada","India",}   #set - unordered and unchangeable
+student = {
+    "firstname":"Esther",
+    "course":"MIT",
+    "age":19,
+    "Nationality":"Kenyan",
+    "Gender":"female"
+}    #dictionary - key value pair
+
+
+print(age)
+print("the length is",length)
+print(fruits)
+print(countries)
+print(student)
+
+#typecasting - converting one datatype to another
+print(float(age))
+print(int(length))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ strings.py
@@ -0,0 +1,15 @@
+greeting = "Hello"
+course = "SOFTWARE ENGINEERING"
+
+
+print(greeting[0])    #index positioning
+print(len(greeting))   #determine length
+print(len(course))
+
+print(course.lower())  #lowercase
+print(greeting.upper())  #uppercase
+
+#string concatenation - joining strings
+print(greeting + course )
+
+
