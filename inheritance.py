76d7ed0e466ccc88f763b264fcd5072242ba7059
essy-njoroge <esthernnjoroge18@gmail.com>
2026-02-12 07:18:24 +0300

    new commit

--- inheritance.py
+++ inheritance.py
@@ -1,5 +1,4 @@
-
-#Parent class/Super class/base class
+# Parent class / Super class / Base class
 
 class Animal:
     isMammal = True
@@ -7,20 +6,29 @@ class Animal:
     def speak(self):
         print("Animal is speaking")
 
-# Child class/sub-class/derived class - borrows attributes/behaviors
 
-class Cat (Animal):
+# Child class / Sub-class / Derived class - borrows attributes/behaviors
+
+class Cat(Animal):
     def meow(self):
         print("Cat is meowing")
 
     def climb(self):
         print("Cat is climbing a tree")
 
-class Donkey:
-    hasTail : True
 
-def move(self):
-    print("Donkey is moving")
+class Donkey(Animal):
+    hasTail = True
+
+    def move(self):
+        print("Donkey is moving")
 
 
+c = Cat()      # create object
+c.speak()      # call inherited method
+c.meow()       # call Cat method
+print()
+d= Donkey()     # create Donkey object
+d.speak()        # inherited from Animal
+d.move()         # Donkey's own method
 
